53f8d5e2a0c2c6833353cd37509117556d5a8833
juliane <jcasier@protonmail.com>
2023-04-27 14:20:22 +0200

    managing global css and home page and board page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 import styles from '@styles/Home.module.css';
 
 // const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const [boardNameEntered, setboardNameEntered] = useState(['']);
+  const createBoard = (e) => {
+    setboardNameEntered(e.target.value);
+  };
+  console.log(boardNameEntered);
   return (
     <>
       <Head>
@@ -17,13 +23,23 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className={styles.main}>
-        <h1 id='logo'>MEMOMO</h1>
+        <h1 id='logo' className={styles.logoHome}>
+          MEMOMO
+        </h1>
         <p>Créez votre tableau</p>
 
-        <input type='text' id='boardName' className={styles.boardNameEnter} />
-        <button type='button' className={styles.plusButton}>
-          +
-        </button>
+        <input
+          type='text'
+          id='boardName'
+          className={styles.boardNameEnter}
+          onBlur={createBoard}
+        />
+        <Link
+          href={`/${boardNameEntered}`}
+          className={`plusButton ${styles.plusButtonHome}`}
+        >
+          &nbsp; + &nbsp;
+        </Link>
       </main>
     </>
   );
