a2309f5cce16a4abbaf8f97df0b4d1d8a9d4b515
juliane <jcasier@protonmail.com>
2023-05-11 17:13:47 +0200

    improving sass removing mixins and using extend instead (more apropriate). Adding a PrimaryLayout wrapper for home page and about page with css and api call (to improve with SSR)

--- pages/index.js
+++ pages/index.js
@@ -2,88 +2,54 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import styles from '@styles/Home.module.scss';
-import { getBoardsRequest, addBoardsRequest } from '../requests/boards';
-import Nav from '../components/nav/index';
+import { addBoardsRequest } from '../requests/boards';
+import PrimaryLayout from '../components/primaryLayout';
 export default function Home() {
   const [boardNameEntered, setboardNameEntered] = useState(['']);
-  const [boardsList, setBoardsList] = useState(null);
 
   const saveBoardName = (e) => {
     setboardNameEntered(e.target.value);
   };
-  // function for API calls
-  const getBoards = async () => {
-    try {
-      const { data } = await getBoardsRequest();
-      setBoardsList(data);
-    } catch (error) {
-      throw error;
-    }
-  };
 
-  // API calls on page loading : loading boards
-  useEffect(() => {
-    getBoards();
-  }, []);
-  useEffect(() => {
-    console.log(boardsList);
-  }, [boardsList]);
   const addBoard = async () => {
     let existingBoardEntered = false;
 
     // if a board with boardNameEntered exists, nothing happens (just the link).
     //if no existing board is found, one is created in database
     //if an existing board exists, the function does nothing, only lik is applied
-    if (boardsList) {
-      boardsList.map((board) => {
-        if (boardNameEntered === board.board_name) {
-          existingBoardEntered = true;
 
-          return;
-        }
-      });
-      if (!existingBoardEntered) {
-        const body = { boardName: boardNameEntered };
-        const res = await addBoardsRequest(body);
+    boardsList.map((board) => {
+      if (boardNameEntered === board.board_name) {
+        existingBoardEntered = true;
+
+        return;
       }
+    });
+    if (!existingBoardEntered) {
+      const body = { boardName: boardNameEntered };
+      const res = await addBoardsRequest(body);
     }
   };
 
   return (
     <>
-      <Head>
-        <title>MEMOMO </title>
-        <meta
-          name='description'
-          content='Création de tableaux personnels contenant des listes'
+      <PrimaryLayout>
+        <p>Créez votre tableau</p>
+
+        <input
+          type='text'
+          id='boardName'
+          className={styles.boardNameEnter}
+          onBlur={saveBoardName}
         />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <div className={styles.mainWrapper}>
-        <Nav boards={boardsList} />
-        <main className={styles.main}>
-          <h1 id='logo' className={styles.logoHome}>
-            MEMOMO
-          </h1>
-          <p>Créez votre tableau</p>
-
-          <input
-            type='text'
-            id='boardName'
-            className={styles.boardNameEnter}
-            onBlur={saveBoardName}
-          />
-          <Link
-            href={`/${boardNameEntered}`}
-            className={styles.plusButtonHome}
-            onClick={addBoard}
-          >
-            &nbsp; + &nbsp;
-          </Link>
-        </main>
-      </div>
+        <Link
+          href={`/${boardNameEntered}`}
+          className={styles.plusButtonHome}
+          onClick={addBoard}
+        >
+          &nbsp; + &nbsp;
+        </Link>
+      </PrimaryLayout>
     </>
   );
 }
